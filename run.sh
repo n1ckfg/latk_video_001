cfe266e22ae1ff3f6b5b5707e3ac43d1f324c63f
Nick Fox-Gieg <nick@fox-gieg.com>
2022-07-19 09:17:03 -0400

    test

--- run.sh
+++ run.sh
@@ -7,26 +7,5 @@ MAX_FRAMES=9999
 RENDER_RES=480 # 480
 
 rm -rf $RESULT_PATH
-python test.py --name $STYLE --dataroot $RGB_PATH --how_many $MAX_FRAMES --size $RENDER_RES
-
-if [ "$1" = "midas" ]
-then
-	echo ""
-	echo "+ + +   MiDaS pass enabled...   + + +"
-	rm -rf $DEPTH_PATH
-	python midas/run.py --input_path $RGB_PATH --output_path $DEPTH_PATH --model_weights midas/model/model-f6b98070.pt 
-fi
-
-LINE_THRESHOLD=64 # 64
-USE_SWIG=1
-INPAINT=0
-
-if [ "$1" = "inpaint" ]
-then
-	echo ""
-	echo "+ + +   Inpainting pass enabled...   + + +"
-	INPAINT=1
-fi
-
-python skeletonizer.py -- $RESULT_PATH $RGB_PATH $DEPTH_PATH $LINE_THRESHOLD $USE_SWIG $INPAINT
+python encoder.py --name $STYLE --dataroot $RGB_PATH --how_many $MAX_FRAMES --size $RENDER_RES
 
