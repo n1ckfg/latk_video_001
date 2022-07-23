87b5c5e24e16472360bcafb15a918b6e65d0b907
Nick Fox-Gieg <nick@fox-gieg.com>
2022-07-22 21:59:20 -0400

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -220,8 +220,7 @@ def main():
         counter += 1
 
     # https://trac.ffmpeg.org/wiki/Encode/H.264
-    # ffmpeg -y -i output%d.png -c:v libx264 -pix_fmt yuvj444p -crf 0 -preset slow -r 30 output.mp4
-    # ffmpeg -y -i output%d.png -c:v libx264 -pix_fmt yuv420p -crf 0 -preset slow -r 30 output.mp4
-    os.system("ffmpeg -y -i " + outputPath + "/output%d.png -c:v libx264 -pix_fmt yuv420p -preset slow -crf 0 -r 30 output/output.mp4")
+    # ffmpeg -y -i output%d.png -c:v libx264 -pix_fmt yuv420p -crf 5 -preset slow -r 30 output.mp4
+    os.system("ffmpeg -y -i " + outputPath + "/output%d.png -c:v libx264 -pix_fmt yuv420p -preset slow -crf 5 -r 30 output/output.mp4")
 
 main()
