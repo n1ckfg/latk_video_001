02628f8249f9d042591c90459ef1c00cf0f09054
Nick Fox-Gieg <nick@fox-gieg.com>
2022-07-24 16:32:36 -0400

    test

--- tools/util.py
+++ tools/util.py
@@ -1,4 +1,23 @@
-def xyFromLoc(loc, width):
+# https://stackoverflow.com/questions/22236956/rgb-to-hsv-via-pil-and-colorsys
+def PilHsvColor(img):
+    if isinstance(img,Image.Image):
+        r,g,b = img.split()
+        Hdat = []
+        Sdat = []
+        Vdat = [] 
+        for rd,gn,bl in zip(r.getdata(),g.getdata(),b.getdata()) :
+            h,s,v = colorsys.rgb_to_hsv(rd/255.,gn/255.,bl/255.)
+            Hdat.append(int(h*255.))
+            Sdat.append(int(s*255.))
+            Vdat.append(int(v*255.))
+        r.putdata(Hdat)
+        g.putdata(Sdat)
+        b.putdata(Vdat)
+        return Image.merge('RGB',(r,g,b))
+    else:
+        return None
+
+        def xyFromLoc(loc, width):
     x = loc % width
     y = (loc - x) / width
     return x, y
