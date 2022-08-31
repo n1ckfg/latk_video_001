abb3479d6dbb658969c66597a88ded432ba29f5d
Nick Fox-Gieg <nick@fox-gieg.com>
2022-08-31 12:31:21 -0400

    set framerate

--- run.bat
+++ run.bat
@@ -5,10 +5,11 @@ set OUTPUT_PATH=output
 set DIM=1024
 set TILE_PIXEL_SIZE=8
 set TILE_SUBDIV=16
+set FRAMERATE=12
 
 del %INPUT_PATH%\*_resampled.ply
 rmdir /s /q %OUTPUT_PATH%
 mkdir %OUTPUT_PATH%
-python encoder.py -- %INPUT_PATH% %OUTPUT_PATH% %DIM% %TILE_PIXEL_SIZE% %TILE_SUBDIV%
+python encoder.py -- %INPUT_PATH% %OUTPUT_PATH% %DIM% %TILE_PIXEL_SIZE% %TILE_SUBDIV% %FRAMERATE%
 
 @pause
